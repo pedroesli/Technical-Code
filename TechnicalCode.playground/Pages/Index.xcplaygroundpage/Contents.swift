624c10aead7f0a22fed27c7aee4bb2d3a58502e5
Pedro Esli <pedroesli.vn@gmail.com>
2023-03-26 19:18:32 -0300

    feat: first push

---
+++ TechnicalCode.playground/Pages/Index.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+/*:
+ # Technical Codes
+ This book is intended to document and save technical coding that I have done for learning, like design patterns and code techniques. Feel free to copy or improve any code.
+ 
+ - Note:
+    If you are reading this from Xcode I highly recommend you to activate "Rendered Markup". To activate go to: **Editor**, then click **Show Rendered Markup**.
+ 
+ ### Index
+ 1. [Linked List](Linked%20List)
+ */
+//: [Next](@next)
---
+++ TechnicalCode.playground/Pages/Linked List.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+/*:
+ # Linked List
+
+ [A|  ] → [B|  ] → [C|  ] → nil
+ 
+ Head: [|  ]
+ 
+ Data: A
+ 
+ Next: pointer to next head
+ 
+ [Previous](@previous)
+ [Next](@next)
+ */
+
+import Foundation
+
